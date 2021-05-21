2c870bb93c78f5afb7e63e0fcfce3db5658dfcef
antonlushnikow <antonlushnikow@gmail.com>
2021-05-21 17:43:57 +0500

    lesson3

--- task4.js
+++ task4.js
@@ -1,46 +1,17 @@
 /*
-Присвоить переменной а значение в промежутке [0..15]. 
-С помощью оператора switch организовать вывод чисел от a до 15.
+Нарисовать пирамиду с помощью console.log, как показано на рисунке, 
+только у вашей пирамиды должно быть 20 рядов, а не 5:
+x
+xx
+xxx
+xxxx
+xxxxx
 */
 
 'use strict';
 
-let a = 7;
+let str = '';
 
-switch (a) {
-    case 0:
-        console.log(0);
-    case 1:
-        console.log(1);
-    case 2:
-        console.log(2);
-    case 3:
-        console.log(3);
-    case 4:
-        console.log(4);
-    case 5:
-        console.log(5);
-    case 6:
-        console.log(6);
-    case 7:
-        console.log(7);
-    case 8:
-        console.log(8);
-    case 9:
-        console.log(9);
-    case 10:
-        console.log(10);
-    case 11:
-        console.log(11);
-    case 12:
-        console.log(12);
-    case 13:
-        console.log(13);
-    case 14:
-        console.log(14);
-    case 15:
-        console.log(15);
-        break;
-    default:
-        console.log('Out of range');
+for (let i = 0; i < 20; i++) {
+    console.log(str += 'x');
 }
