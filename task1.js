3bb44198c3cdcbb970d4f51030b5c74b76bada5c
antonlushnikow <antonlushnikow@gmail.com>
2021-05-29 10:09:00 +0500

    lesson4

--- task1.js
+++ task1.js
@@ -1,14 +1,33 @@
 /*
-С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+Написать функцию, преобразующую число в объект. Передавая на вход число от 0 до 999, 
+надо получить на выходе объект, в котором в соответствующих свойствах описаны единицы, 
+десятки и сотни. Например, для числа 245 надо получить следующий объект: 
+{‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. Если число превышает 999, 
+необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
 */
 
-'use strict';
+'use strict'
 
-outer: for (let i = 2; i < 101; i++) {
-    for (let j = 2; j < i/2 + 1; j++) {
-        if (i % j === 0) {
-            continue outer;
-        }     
-    }
-    console.log(i);
+function Number (ones, tens, hundreds) {
+    this.ones = ones;
+    this.tens = tens;
+    this.hundreds = hundreds;
 }
+
+
+function numPlaces(num) {
+    if (num > 999) {
+        console.log('Число превышает 999');
+        return {};
+    }
+    else {
+        let ones = num % 10;
+        let tens = ((num - ones) / 10) % 10;
+        let hundreds = (num - tens * 10 - ones) / 100;
+        return new Number(ones, tens, hundreds);
+    }
+}
+
+
+console.log(numPlaces(123));  // Number { ones: 3, tens: 2, hundreds: 1 }
+console.log(numPlaces(1223));  // Число превышает 999 {}
--- task2.js
+++ task2.js
@@ -1,27 +1,65 @@
 /*
-С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. 
-Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
-Товары в корзине хранятся в массиве. Задачи:
-a) Организовать такой массив для хранения товаров в корзине;
-b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+Продолжить работу с интернет-магазином:
+В прошлом домашнем задании вы реализовали корзину на базе массивов. Какими объектами можно заменить их элементы?
+Реализуйте такие объекты.
+Перенести функционал подсчета корзины на объектно-ориентированную базу.
 */
 
 'use strict';
 
-let basket = [
-    {name: 'name1', price: 1000},
-    {name: 'name2', price: 2000},
-    {name: 'name3', price: 4000},
-    {name: 'name4', price: 500}
-];
 
-
-function countBasketPrice(arr) {
-    let sum = arr.reduce(function(acc, elem) {
-        return acc + elem.price;
-    }, 0);
-    return sum;
+class Product {
+    constructor (name, price) {
+        this.name = name;
+        this.price = price;
+    }
 }
 
 
-console.log(countBasketPrice(basket));
+class Basket {
+    constructor (...products) {
+        this.products = products;
+    }
+
+    showBasket() {
+        console.log(this.products);
+    }
+
+    getFullPrice() {
+        let sum = this.products.reduce(function(acc, elem) {
+            return acc + elem[0].price * elem[1];
+        }, 0);
+        return sum;
+    }
+}
+
+let product1 = new Product('iPhone 6S', 120000);
+let product2 = new Product('Xiaomi Mi 9', 20000);
+let product3 = new Product('Samsung Galaxy S7', 100000);
+
+let basket1 = new Basket ([product1, 2], [product3, 1]);
+let basket2 = new Basket ([product1, 1], [product2, 2]);
+
+
+basket1.showBasket();
+
+/*
+
+[
+  [ Product { name: 'iPhone 6S', price: 120000 }, 2 ],
+  [ Product { name: 'Samsung Galaxy S7', price: 100000 }, 1 ]
+]
+
+*/
+
+basket2.showBasket();
+
+/*
+[
+  [ Product { name: 'iPhone 6S', price: 120000 }, 1 ],
+  [ Product { name: 'Xiaomi Mi 9', price: 20000 }, 2 ]
+]
+*/
+
+console.log(basket1.getFullPrice());  // 340000
+console.log(basket2.getFullPrice());  // 160000
